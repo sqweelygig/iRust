9560240676e9524925ebea2553a6cf03a4b92ef1
Andrew Lucas <sqweelygig@googlemail.com>
2018-12-23 15:54:25 +0000

    wip

--- src/start.ts
+++ src/start.ts
@@ -1,11 +1,3 @@
-import * as epd from "epd7x5";
-
-epd.init();
-
-const image = epd.getImageBuffer();
-
-console.log(image.height);
-
-image.filledRectangle(0, 220, 640, 310, epd.white);
-
-epd.displayImageBuffer(image);
+import * as Express from "express";
+// Just a file.
+Express();
