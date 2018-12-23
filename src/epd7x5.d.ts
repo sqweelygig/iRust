3b345a8906155ce0addc9d093e959f6ff7c78fac
Andrew Lucas <sqweelygig@googlemail.com>
2018-12-23 14:17:58 +0000

    wip

--- src/epd7x5.d.ts
+++ src/epd7x5.d.ts
@@ -1,3 +1,9 @@
+interface NodeGD {
+	getGDVersion(): string;
+}
+
 declare module "epd7x5" {
 	function init(): void;
+	function getImageBuffer(): NodeGD;
+	function displayImageBuffer(frame: NodeGD): void;
 }
--- src/start.ts
+++ src/start.ts
@@ -1,3 +1,7 @@
 import * as epd from "epd7x5";
 
 epd.init();
+
+const frame = epd.getImageBuffer();
+
+epd.displayImageBuffer(frame);
