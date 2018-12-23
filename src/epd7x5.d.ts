649118a22399cddf8f9940bafc53121c1b9ad541
Andrew Lucas <sqweelygig@googlemail.com>
2018-12-23 14:49:44 +0000

    wip

--- src/epd7x5.d.ts
+++ src/epd7x5.d.ts
@@ -1,8 +1,10 @@
 interface ImageBuffer {
 	height: number;
+	filledRectangle(bottom: number, left: number, top: number, right: number, colour: number): void;
 }
 
 declare module "epd7x5" {
+	const white: number;
 	function init(): void;
 	function getImageBuffer(): ImageBuffer;
 	function displayImageBuffer(frame: ImageBuffer): void;
--- src/start.ts
+++ src/start.ts
@@ -5,3 +5,7 @@ epd.init();
 const image = epd.getImageBuffer();
 
 console.log(image.height);
+
+image.filledRectangle(0, 220, 640, 310, epd.white);
+
+epd.displayImageBuffer(image);
