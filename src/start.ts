67d23c388f6e2a2f5cd1816d214804dda44243c8
Andrew Lucas <sqweelygig@googlemail.com>
2018-12-24 16:45:47 +0000

    wip

--- src/start.ts
+++ src/start.ts
@@ -2,20 +2,30 @@
 
 import * as rpio from "rpio";
 
-const COMMAND = 0x6000;
+// const COMMAND = ;
 // const WRITE_DATA = 0x0000;
 
-const FULL_IMAGE = 0x0020;
-const END_IMAGE = 0x0022;
+// const FULL_IMAGE = 0x20;
+// const END_IMAGE = 0x22;
 
 // const WIDTH = 1200;
 // const HEIGHT = 825;
 
-const START_UPDATE = Buffer.from([COMMAND, FULL_IMAGE]);
-const END_UPDATE = Buffer.from([COMMAND, END_IMAGE]);
+// const EMPTY = 0x00;
 
-rpio.spiBegin();
-rpio.spiSetClockDivider(32);
-rpio.spiWrite(START_UPDATE, START_UPDATE.length);
-rpio.spiWrite(END_UPDATE, START_UPDATE.length);
-rpio.spiEnd();
+const START_UPDATE = Buffer.from([0x60, 0x00, 0x00, 0x20]);
+const END_UPDATE = Buffer.from([0x60, 0x00, 0x00, 0x22]);
+
+// const DATA_PREFIX = [0x00, 0x00];
+
+function write(_data: number[]) {
+	// const payload = Buffer.from(DATA_PREFIX.concat(data));
+	rpio.spiBegin();
+	rpio.spiSetClockDivider(32);
+	rpio.spiWrite(START_UPDATE, START_UPDATE.length);
+	// rpio.spiWrite(payload, payload.length);
+	rpio.spiWrite(END_UPDATE, START_UPDATE.length);
+	rpio.spiEnd();
+}
+
+write([]);
