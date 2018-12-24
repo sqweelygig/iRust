58332cd0e53785e78a8e6ded73fa633e26b39f8a
Andrew Lucas <sqweelygig@googlemail.com>
2018-12-24 16:21:50 +0000

    wip

--- src/start.ts
+++ src/start.ts
@@ -3,13 +3,13 @@
 import * as rpio from "rpio";
 
 const COMMAND = 0x6000;
-const WRITE_DATA = 0x0000;
+// const WRITE_DATA = 0x0000;
 
 const FULL_IMAGE = 0x0020;
 const END_IMAGE = 0x0022;
 
-const WIDTH  = 1200;
-const HEIGHT = 825;
+// const WIDTH = 1200;
+// const HEIGHT = 825;
 
 const START_UPDATE = new Buffer([COMMAND, FULL_IMAGE]);
 const END_UPDATE = new Buffer([COMMAND, END_IMAGE]);
