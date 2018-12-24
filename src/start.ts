a6b3d671bb9aa8f9a5546190b21e292299fd9c35
Andrew Lucas <sqweelygig@googlemail.com>
2018-12-24 13:59:01 +0000

    wip

--- src/start.ts
+++ src/start.ts
@@ -1,4 +1,11 @@
 import * as Express from "express";
-// Just a file.
+import * as rpio from "rpio";
+
+console.log("Creating a Express instance, to keep the process alive.");
 const express = Express();
 express.listen(process.env.PORT || 80);
+
+console.log("rpio.spiBegin");
+rpio.spiBegin();
+console.log("rpio.spiEnd");
+rpio.spiEnd();
