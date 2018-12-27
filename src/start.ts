151690a44b828beef591d9bebecf67c457b74cf6
Andrew Lucas <sqweelygig@googlemail.com>
2018-12-27 10:28:15 +0000

    wip

--- src/start.ts
+++ src/start.ts
@@ -42,6 +42,6 @@ function write(data: number[]) {
 	rpio.spiEnd();
 }
 
-write(new Array<number>(WIDTH * HEIGHT).fill(255));
+write(new Array<number>(WIDTH * HEIGHT).fill(0xffffffff));
 
 rpio.sleep(300);
