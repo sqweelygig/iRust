a02c16bfb4224b22c93fc351cd719b40b162e777
Andrew Lucas <sqweelygig@googlemail.com>
2018-12-27 11:13:27 +0000

    wip

--- it8951/main.c
+++ it8951/main.c
@@ -8,34 +8,11 @@ int main (int argc, char *argv[])
 	printf("ReadReg = 0x%x\n",IT8951ReadReg(LISAR));
 	*/
 
-	bcm2835_init();
-
-	bcm2835_spi_begin();
-	bcm2835_spi_setBitOrder(BCM2835_SPI_BIT_ORDER_MSBFIRST);   	//default
-	bcm2835_spi_setDataMode(BCM2835_SPI_MODE0);               		//default
-	bcm2835_spi_setClockDivider(BCM2835_SPI_CLOCK_DIVIDER_32);		//default
-
-	bcm2835_gpio_fsel(CS, BCM2835_GPIO_FSEL_OUTP);
-	bcm2835_gpio_fsel(HRDY, BCM2835_GPIO_FSEL_INPT);
-	bcm2835_gpio_fsel(RESET, BCM2835_GPIO_FSEL_OUTP);
-
-	bcm2835_gpio_write(CS, HIGH);
-
-	printf("****** IT8951(2) ******\n");
-
-	bcm2835_gpio_write(RESET, LOW);
-	bcm2835_delay(100);
-	bcm2835_gpio_write(RESET, HIGH);
-
-	//Get Device Info
-	GetIT8951SystemInfo(&gstI80DevInfo);
-
-	gpFrameBuf = malloc(gstI80DevInfo.usPanelW * gstI80DevInfo.usPanelH);
-
- 	gulImgBufAddr = gstI80DevInfo.usImgBufAddrL | (gstI80DevInfo.usImgBufAddrH << 16);
-
- 	//Set to Enable I80 Packed mode
- 	IT8951WriteReg(I80CPCR, 0x0001);
+	if(IT8951_Init())
+	{
+		printf("IT8951_Init error \n");
+		return 1;
+	}
 
 	//IT8951DisplayExample();
 	//IT8951DisplayExample2();
