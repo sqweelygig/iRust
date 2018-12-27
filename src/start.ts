b880bd8ec934dc423a0dc7fbdc6b455c9f1865dc
Andrew Lucas <sqweelygig@googlemail.com>
2018-12-27 16:20:59 +0000

    wip

--- src/start.ts
+++ src/start.ts
@@ -41,6 +41,7 @@ const txArray = new Array(size).fill(0x00);
 txArray[0] = 0x10;
 const txBuffer = Buffer.from(txArray);
 rpio.spiTransfer(txBuffer, rxBuffer, COMMAND.RX_DATA.length);
+console.log(0x04b0);
 
 rpio.spiEnd();
 
