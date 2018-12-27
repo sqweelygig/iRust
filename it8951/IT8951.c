031f3684cdddaf2ed4f13157a78466be62497520
Andrew Lucas <sqweelygig@googlemail.com>
2018-12-27 11:59:31 +0000

    wip

--- it8951/IT8951.c
+++ it8951/IT8951.c
@@ -530,6 +530,7 @@ uint8_t IT8951_Init()
 	bcm2835_gpio_write(CS, HIGH);
 
 	printf("****** IT8951 ******\n");
+	printf((unsigned)time(NULL));
 	
 	bcm2835_gpio_write(RESET, LOW);
 	bcm2835_delay(100);
@@ -723,8 +724,6 @@ void IT8951_GUI_Example()
 
 	//memset(gpFrameBuf, 0xff, gstI80DevInfo.usPanelW * gstI80DevInfo.usPanelH);
 	EPD_Clear(0xff);
-
-	printf((unsigned)time(NULL));
 
 	EPD_DrawMatrix(0,0,550,412,bmp01);
 
