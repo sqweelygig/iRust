7b55d48d05728ec2e23b9598d63b7d3dbae9da17
Andrew Lucas <sqweelygig@googlemail.com>
2018-12-27 09:59:59 +0000

    wip

--- src/start.ts
+++ src/start.ts
@@ -16,20 +16,28 @@ const HEIGHT = 825;
 const START_UPDATE = Buffer.from([0x60, 0x00, 0x00, 0x20]);
 const END_UPDATE = Buffer.from([0x60, 0x00, 0x00, 0x22]);
 
-const DATA_PREFIX = [0x00, 0x00];
-
 function write(data: number[]) {
-	const byteDigestedData: number[] = [];
+	const byteDigestedData = [0x00, 0x00];
 	data.forEach((datum) => {
 		byteDigestedData.push(Math.floor(datum / 256));
 		byteDigestedData.push(datum % 256);
 	});
-	const payload = Buffer.from(DATA_PREFIX.concat(byteDigestedData));
+	const payload = Buffer.from(byteDigestedData);
 	rpio.spiBegin();
 	rpio.spiSetClockDivider(32);
+	rpio.open(24, rpio.OUTPUT, rpio.HIGH);
+	rpio.msleep(500);
+	rpio.write(24, rpio.LOW);
 	rpio.spiWrite(START_UPDATE, START_UPDATE.length);
+	rpio.write(25, rpio.HIGH);
+	rpio.msleep(500);
+	rpio.write(24, rpio.LOW);
 	rpio.spiWrite(payload, payload.length);
+	rpio.write(25, rpio.HIGH);
+	rpio.msleep(500);
+	rpio.write(24, rpio.LOW);
 	rpio.spiWrite(END_UPDATE, START_UPDATE.length);
+	rpio.write(25, rpio.HIGH);
 	rpio.spiEnd();
 }
 
