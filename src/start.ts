a354122139cc0ecd17c1d677987bd11e9f1b2589
Andrew Lucas <sqweelygig@googlemail.com>
2018-12-27 10:39:07 +0000

    wip

--- src/start.ts
+++ src/start.ts
@@ -14,32 +14,33 @@ const HEIGHT = 825;
 
 // const EMPTY = 0x00;
 
-const START_UPDATE = Buffer.from([0x60, 0x00, 0x00, 0x20]);
-const END_UPDATE = Buffer.from([0x60, 0x00, 0x00, 0x22]);
+// const START_UPDATE = Buffer.from([0x60, 0x00, 0x00, 0x20]);
+// const END_UPDATE = Buffer.from([0x60, 0x00, 0x00, 0x22]);
+// const UPDATE_DISPLAY = Buffer.from([0x60, 0x00, 0x00, 0x34]);
 
-function write(data: number[]) {
-	const byteDigestedData = [0x00, 0x00];
-	data.forEach((datum) => {
-		byteDigestedData.push(Math.floor(datum / 256));
-		byteDigestedData.push(datum % 256);
-	});
-	const payload = Buffer.from(byteDigestedData);
-	rpio.spiBegin();
-	rpio.spiSetClockDivider(32);
-	rpio.open(24, rpio.OUTPUT, rpio.HIGH);
-	rpio.msleep(500);
-	rpio.write(24, rpio.LOW);
-	rpio.spiWrite(START_UPDATE, START_UPDATE.length);
-	rpio.write(24, rpio.HIGH);
-	rpio.msleep(500);
-	rpio.write(24, rpio.LOW);
-	rpio.spiWrite(payload, payload.length);
-	rpio.write(24, rpio.HIGH);
-	rpio.msleep(500);
-	rpio.write(24, rpio.LOW);
-	rpio.spiWrite(END_UPDATE, START_UPDATE.length);
-	rpio.write(24, rpio.HIGH);
-	rpio.spiEnd();
+function write(_data: number[]) {
+	// const byteDigestedData = [0x00, 0x00];
+	// data.forEach((datum) => {
+	// 	byteDigestedData.push(Math.floor(datum / 256));
+	// 	byteDigestedData.push(datum % 256);
+	// });
+	// const payload = Buffer.from(byteDigestedData);
+	// rpio.spiBegin();
+	// rpio.spiSetClockDivider(32);
+	// rpio.open(24, rpio.OUTPUT, rpio.HIGH);
+	// rpio.msleep(500);
+	// rpio.write(24, rpio.LOW);
+	// rpio.spiWrite(START_UPDATE, START_UPDATE.length);
+	// rpio.write(24, rpio.HIGH);
+	// rpio.msleep(500);
+	// rpio.write(24, rpio.LOW);
+	// rpio.spiWrite(payload, payload.length);
+	// rpio.write(24, rpio.HIGH);
+	// rpio.msleep(500);
+	// rpio.write(24, rpio.LOW);
+	// rpio.spiWrite(END_UPDATE, START_UPDATE.length);
+	// rpio.write(24, rpio.HIGH);
+	// rpio.spiEnd();
 }
 
 write(new Array<number>(WIDTH * HEIGHT).fill(0x00000000));
