be273fdfb68e39ba3b6fe444c76754b7110be6a6
Andrew Lucas <sqweelygig@googlemail.com>
2018-12-27 16:30:56 +0000

    wip

--- src/start.ts
+++ src/start.ts
@@ -26,7 +26,6 @@ rpio.open(PIN.RESET, rpio.OUTPUT, rpio.HIGH);
 rpio.open(PIN.READY, rpio.INPUT);
 rpio.spiBegin();
 rpio.spiSetClockDivider(32);
-
 rpio.write(PIN.RESET, rpio.LOW);
 rpio.msleep(100);
 rpio.write(PIN.RESET, rpio.HIGH);
@@ -34,7 +33,6 @@ awaitDisplayReady();
 
 rpio.spiWrite(COMMAND.GET_INFO, COMMAND.GET_INFO.length);
 awaitDisplayReady();
-
 const size = 42;
 const rxBuffer = Buffer.alloc(size);
 rpio.spiTransfer(COMMAND.RX_DATA, rxBuffer, size);
@@ -42,59 +40,5 @@ rpio.spiTransfer(COMMAND.RX_DATA, rxBuffer, size);
 rpio.spiEnd();
 
 console.log(rxBuffer);
-console.log(rxBuffer.readInt16BE(2));
-console.log(rxBuffer.readInt16BE(3));
-//
-// const START_UPDATE = Buffer.from([0x60, 0x00, 0x00, 0x20]);
-// const END_UPDATE = Buffer.from([0x60, 0x00, 0x00, 0x22]);
-// const UPDATE_DISPLAY = Buffer.from([0x60, 0x00, 0x00, 0x34]);
-// const DEFINE_AREA = Buffer.from([
-// 	0x00,
-// 	0x00,
-// 	0x00,
-// 	0x00,
-// 	Math.floor(WIDTH / 256),
-// 	WIDTH % 256,
-// 	Math.floor(HEIGHT / 256),
-// 	HEIGHT % 256,
-// 	0x00,
-// 	2,
-// ]);
-//
-// function write(data: number[]) {
-// 	const byteDigestedData = [0x00, 0x00];
-// 	data.forEach((datum) => {
-// 		byteDigestedData.push(Math.floor(datum / 256));
-// 		byteDigestedData.push(datum % 256);
-// 	});
-// 	const payload = Buffer.from(byteDigestedData);
-// 	rpio.spiBegin();
-// 	rpio.spiSetClockDivider(32);
-// 	rpio.open(24, rpio.OUTPUT, rpio.HIGH);
-// 	rpio.write(24, rpio.HIGH);
-// 	rpio.msleep(500);
-// 	rpio.write(24, rpio.LOW);
-// 	rpio.spiWrite(START_UPDATE, START_UPDATE.length);
-// 	rpio.write(24, rpio.HIGH);
-// 	rpio.msleep(500);
-// 	rpio.write(24, rpio.LOW);
-// 	rpio.spiWrite(payload, payload.length);
-// 	rpio.write(24, rpio.HIGH);
-// 	rpio.msleep(500);
-// 	rpio.write(24, rpio.LOW);
-// 	rpio.spiWrite(END_UPDATE, START_UPDATE.length);
-// 	rpio.write(24, rpio.HIGH);
-// 	rpio.msleep(500);
-// 	rpio.write(24, rpio.LOW);
-// 	rpio.spiWrite(UPDATE_DISPLAY, UPDATE_DISPLAY.length);
-// 	rpio.write(24, rpio.HIGH);
-// 	rpio.msleep(500);
-// 	rpio.write(24, rpio.LOW);
-// 	rpio.spiWrite(DEFINE_AREA, DEFINE_AREA.length);
-// 	rpio.write(24, rpio.HIGH);
-// 	rpio.spiEnd();
-// }
-//
-// write(new Array<number>(WIDTH * HEIGHT).fill(0x00000000));
-//
-// rpio.sleep(300);
+console.log(rxBuffer.readInt16BE(4));
+console.log(rxBuffer.readInt16BE(6));
