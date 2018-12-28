4b0f1e336e0b5936526942bcb38e0c164d44c0f1
Andrew Lucas <sqweelygig@googlemail.com>
2018-12-28 17:46:59 +0000

    wip

--- src/start.ts
+++ src/start.ts
@@ -144,43 +144,42 @@ class Display {
 }
 
 async function test() {
+	const rightNow = moment();
 	const display = await Display.build();
 	const spec = display.getDimensions();
 	const radius = Math.floor(Math.min(spec.width, spec.height) / 2);
-	setInterval(async () => {
-		const rightNow = moment();
-		const stage = await display.createStage(0xffffff);
-		stage.ellipse(radius, radius, radius * 2, radius * 2, 0x000000);
-		stage.line(
-			radius,
-			radius,
-			radius -
-				Math.round(radius * Math.sin((rightNow.seconds() * Math.PI) / 30)),
-			radius +
-				Math.round(radius * Math.cos((rightNow.seconds() * Math.PI) / 30)),
-			0x000000,
-		);
-		stage.line(
-			radius,
-			radius,
-			radius -
-				Math.round(radius * Math.sin((rightNow.minutes() * Math.PI) / 30)),
-			radius +
-				Math.round(radius * Math.cos((rightNow.minutes() * Math.PI) / 30)),
-			0x000000,
-		);
-		stage.line(
-			radius,
-			radius,
-			radius -
-				Math.round(0.7 * radius * Math.sin((rightNow.hour() * Math.PI) / 6)),
-			radius +
-				Math.round(0.7 * radius * Math.cos((rightNow.hour() * Math.PI) / 6)),
-			0x000000,
-		);
-		console.log(rightNow.hour(), rightNow.minutes(), rightNow.seconds());
-		await display.sendStage(stage);
-	}, 1000);
+	const stage = await display.createStage(0xffffff);
+	stage.ellipse(radius, radius, radius * 2, radius * 2, 0x000000);
+	stage.line(
+		radius,
+		radius,
+		radius -
+			Math.round(radius * Math.sin((rightNow.seconds() * Math.PI) / 30)),
+		radius +
+			Math.round(radius * Math.cos((rightNow.seconds() * Math.PI) / 30)),
+		0x000000,
+	);
+	stage.line(
+		radius,
+		radius,
+		radius -
+			Math.round(radius * Math.sin((rightNow.minutes() * Math.PI) / 30)),
+		radius +
+			Math.round(radius * Math.cos((rightNow.minutes() * Math.PI) / 30)),
+		0x000000,
+	);
+	stage.line(
+		radius,
+		radius,
+		radius -
+			Math.round(0.7 * radius * Math.sin((rightNow.hour() * Math.PI) / 6)),
+		radius +
+			Math.round(0.7 * radius * Math.cos((rightNow.hour() * Math.PI) / 6)),
+		0x000000,
+	);
+	console.log(rightNow.hour(), rightNow.minutes(), rightNow.seconds());
+	await display.sendStage(stage);
+	display.destructor();
 }
 
 test().then(() => {
