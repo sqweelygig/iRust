bef689a7d82c2d82d8105d1fa457ea7ff384abe1
Andrew Lucas <sqweelygig@googlemail.com>
2018-12-28 12:03:45 +0000

    wip

--- it8951/IT8951.c
+++ it8951/IT8951.c
@@ -752,7 +752,7 @@ void IT8951_GUI_Example()
 //	EPD_DrawBitmap(0,0,bmp01);
 
 	//EPD_DrawMatrix(1,1,550,412,bmp01);
-	EPD_DrawMatrix(300,000,550,412,bmp01);
+	EPD_DrawMatrix(100,100,550,412,bmp01);
 
 
 	//��ʾͼ��
