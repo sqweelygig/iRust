7638fdaf3fe8d94120da218592d66a90150dace2
Andrew Lucas <sqweelygig@googlemail.com>
2018-12-30 11:47:39 +0000

    wip

--- src/start.ts
+++ src/start.ts
@@ -2,31 +2,33 @@ import * as moment from "moment";
 import * as gd from "node-gd";
 import { Display, DisplayDimensions } from "./display";
 
-async function createStage(
-	dimensions: DisplayDimensions,
-	fill?: number,
-): Promise<Stage> {
-	return new Promise<Stage>((resolve, reject) => {
-		gd.createTrueColor(dimensions.width, dimensions.height, (error, stage) => {
-			if (error) {
-				reject(error);
-			} else if (stage) {
-				if (fill) {
-					stage.fill(0, 0, fill);
+class Page {
+	public static async createStage(
+		dimensions: DisplayDimensions,
+		fill?: number,
+	): Promise<Stage> {
+		return new Promise<Stage>((resolve, reject) => {
+			gd.createTrueColor(dimensions.width, dimensions.height, (error, stage) => {
+				if (error) {
+					reject(error);
+				} else if (stage) {
+					if (fill) {
+						stage.fill(0, 0, fill);
+					}
+					resolve(stage);
+				} else {
+					reject();
 				}
-				resolve(stage);
-			} else {
-				reject();
-			}
+			});
 		});
-	});
+	}
 }
 
 async function startClock(display: Display) {
 	const dimensions = display.getDimensions();
 	// noinspection InfiniteLoopJS
 	while (true) {
-		const stage = await createStage(dimensions, 0xffffff);
+		const stage = await Page.createStage(dimensions, 0xffffff);
 		const now = moment();
 		stage.stringFT(
 			0x000000,
