d0c76f74d428c5f00a973c3c18e3a994bfa20511
Andrew Lucas <sqweelygig@googlemail.com>
2018-12-30 11:45:11 +0000

    wip

--- src/start.ts
+++ src/start.ts
@@ -2,40 +2,38 @@ import * as moment from "moment";
 import * as gd from "node-gd";
 import { Display, DisplayDimensions } from "./display";
 
-class Page {
-	public static async build(
-		dimensions: DisplayDimensions,
-		fill?: number,
-	): Promise<Stage> {
-		return new Promise<Stage>((resolve, reject) => {
-			gd.createTrueColor(dimensions.width, dimensions.height, (error, stage) => {
-				if (error) {
-					reject(error);
-				} else if (stage) {
-					if (fill) {
-						stage.fill(0, 0, fill);
-					}
-					resolve(stage);
-				} else {
-					reject();
+async function createStage(
+	dimensions: DisplayDimensions,
+	fill?: number,
+): Promise<Stage> {
+	return new Promise<Stage>((resolve, reject) => {
+		gd.createTrueColor(dimensions.width, dimensions.height, (error, stage) => {
+			if (error) {
+				reject(error);
+			} else if (stage) {
+				if (fill) {
+					stage.fill(0, 0, fill);
 				}
-			});
+				resolve(stage);
+			} else {
+				reject();
+			}
 		});
-	}
+	});
 }
 
 async function startClock(display: Display) {
 	const dimensions = display.getDimensions();
 	// noinspection InfiniteLoopJS
 	while (true) {
-		const stage = await Page.build(dimensions, 0xffffff);
+		const stage = await createStage(dimensions, 0xffffff);
 		const now = moment();
 		stage.stringFT(
 			0x000000,
 			"/usr/src/imuse/lib/seven-segment.ttf",
 			64,
 			Math.PI / 2,
-			5000,
+			1000,
 			400,
 			now.format("hh:mm"),
 		);
