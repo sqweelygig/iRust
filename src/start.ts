ab3135f7c13fb99c7c654587b2d1aa98b58f12ad
Andrew Lucas <sqweelygig@googlemail.com>
2018-12-30 16:26:18 +0000

    wip

--- src/start.ts
+++ src/start.ts
@@ -4,24 +4,27 @@ import { DataRepository } from "./data-repository";
 import { Display } from "./display";
 import { Page } from "./page";
 
-async function start() {
+async function start(defaultTextStyle) {
 	let previousContent: string | null = null;
 	const onUpdate = async () => {
 		console.log("Data Repository Updated.");
-		const content = await data.get(Path.join("content", `${process.env.PAGE}.md`));
+		const content = await data.get(
+			Path.join("content", `${process.env.PAGE}.md`),
+		);
 		if (previousContent !== content) {
 			previousContent = content;
+			const page = await Page.build(
+				display.getDimensions(),
+				defaultTextStyle,
+				0xffffff,
+			);
+			content.split(/\r\n/g).forEach((line) => {
+				page.write(line);
+			});
+			await display.update(page);
 			console.log(content);
 			console.log("Content Updated.");
 		}
-	};
-	const defaultTextStyle = {
-		colour: 0x000000,
-		fontPath: "/usr/src/imuse/lib/sassoon-primary.otf",
-		lineDrop: 0.2,
-		lineHeight: 1.16,
-		size: 64,
-		spacing: 10,
 	};
 	const repo = process.env.REPO || "localhost";
 	const data = new DataRepository(repo, onUpdate);
@@ -32,21 +35,16 @@ async function start() {
 	console.log("Display Panel Initialised.");
 	await onUpdate();
 	console.log("Initial Content Displayed.");
-	// noinspection InfiniteLoopJS
-	while (true) {
-		const page = await Page.build(
-			display.getDimensions(),
-			defaultTextStyle,
-			0xffffff,
-		);
-		const now = moment();
-		page.write(now.format("dddd MMMM Do, YYYY"));
-		page.write(now.format("hh:mm").toUpperCase(), { size: 128 });
-		await display.update(page);
-	}
 }
 
-start()
+start({
+	colour: 0x000000,
+	fontPath: "/usr/src/imuse/lib/sassoon-primary.otf",
+	lineDrop: 0.2,
+	lineHeight: 1.16,
+	size: 64,
+	spacing: 10,
+})
 	.then(() => {
 		console.log("Application Initialised.");
 	})
