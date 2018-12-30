f25966675ec35ba8639b2193fa2257d43e5b1154
Andrew Lucas <sqweelygig@googlemail.com>
2018-12-30 10:17:35 +0000

    wip

--- src/page.ts
+++ src/page.ts
@@ -2,20 +2,27 @@ import * as gd from "node-gd";
 import { DisplayDimensions } from "./display";
 
 export class Page extends Stage {
-	public static build(dimensions: DisplayDimensions, fill?: number): Promise<Page> {
-		return new Promise<Stage>((resolve, reject) => {
-			gd.createTrueColor(dimensions.width, dimensions.height, (error, stage) => {
-				if (error) {
-					reject(error);
-				} else if (stage) {
-					if (fill) {
-						stage.fill(0, 0, fill);
+	public static build(
+		dimensions: DisplayDimensions,
+		fill?: number,
+	): Promise<Page> {
+		return new Promise<Page>((resolve, reject) => {
+			gd.createTrueColor(
+				dimensions.width,
+				dimensions.height,
+				(error, page) => {
+					if (error) {
+						reject(error);
+					} else if (page) {
+						if (fill) {
+							page.fill(0, 0, fill);
+						}
+						resolve(page);
+					} else {
+						reject();
 					}
-					resolve(stage);
-				} else {
-					reject();
-				}
-			});
+				},
+			);
 		});
 	}
 }
