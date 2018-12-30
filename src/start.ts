9b9780af23bee440c6c61cdb45e9808b516cbf6e
Andrew Lucas <sqweelygig@googlemail.com>
2018-12-30 13:39:02 +0000

    wip

--- src/start.ts
+++ src/start.ts
@@ -79,8 +79,8 @@ async function startClock(display: Display) {
 			{
 				colour: 0x000000,
 				fontPath: "/usr/src/imuse/lib/sassoon-primary.otf",
-				lineDrop: 0.24,
-				lineHeight: 1.16,
+				lineDrop: 0.3,
+				lineHeight: 1.1,
 				size: 64,
 			},
 			0xffffff,
