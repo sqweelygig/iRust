6dbb919da48e58e6efcad42e5712da165d58b8fe
Andrew Lucas <sqweelygig@googlemail.com>
2018-12-31 16:12:13 +0000

    wip

--- src/start.ts
+++ src/start.ts
@@ -7,14 +7,31 @@ import { Display } from "./display";
 import { Page, TextStyle } from "./page";
 
 async function start(
-	defaultTextStyle: TextStyle,
-	textStyles: Dictionary<Partial<TextStyle>>,
+	repo: string,
+	article: string,
 ) {
+	const defaultTextStyle = {
+		colour: 0x000000,
+			fontPath: "/usr/src/imuse/lib/sassoon-primary.otf",
+		lineDrop: 0.2,
+		lineHeight: 1.16,
+		size: 32,
+		spacing: 10,
+	};
+	const textStyles: Dictionary<Partial<TextStyle>> = {
+		h1: {
+			size: 64,
+		},
+		h2: {
+			lineHeight: 1.8,
+				size: 48,
+		},
+	};
 	let previousContent: string | null = null;
 	const onUpdate = async () => {
 		console.log("Data Repository Updated.");
 		const content = await data.get(
-			Path.join("content", `${process.env.PAGE}.md`),
+			Path.join("content", `${article}.md`),
 		);
 		if (previousContent !== content) {
 			previousContent = content;
@@ -39,7 +56,6 @@ async function start(
 			console.log("Content Updated.");
 		}
 	};
-	const repo = process.env.REPO || "localhost";
 	const data = new DataRepository(repo, onUpdate);
 	console.log("Data Repository Initialised.");
 	await data.clone();
@@ -51,28 +67,13 @@ async function start(
 }
 
 start(
-	{
-		colour: 0x000000,
-		fontPath: "/usr/src/imuse/lib/sassoon-primary.otf",
-		lineDrop: 0.2,
-		lineHeight: 1.16,
-		size: 32,
-		spacing: 10,
-	},
-	{
-		h1: {
-			size: 64,
-		},
-		h2: {
-			lineHeight: 1.8,
-			size: 48,
-		},
-	},
+	process.env.REPO || "localhost",
+	process.env.PAGE || "home",
 )
 	.then(() => {
 		console.log("Application Initialised.");
 	})
-	.catch((error) => {
+	.catch((error: Error) => {
 		console.error("Initialisation Failed.");
 		console.error(error);
 	});
